bb259652bcd35f7a7ba5267632faa54fa7f7c74a
Kaiser <kaiserzulkarnain@gmail.com>
2024-04-17 19:40:12 +0700

    refactor: change files name

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
-import HomeHeader from "@app/components/home/HomeHeader";
-import HomeProduct from "@app/components/home/HomeProduct";
+import HomeHeader from "@app/components/home/home-header";
+import HomeProduct from "@app/components/home/home-product";
 
 export default function Home() {
   return (
