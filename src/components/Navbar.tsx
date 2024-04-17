415462f8b02478b5d0090b979d9bc44e7fcc8809
Kaiser <kaiserzulkarnain@gmail.com>
2024-04-17 18:49:48 +0700

    feat: navbar hamburger menu

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,8 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import { CartIcon, Logo } from "./svg";
 import Link from "next/link";
+import { Squash as Hamburger } from "hamburger-react";
 
 const Navbar = () => {
   type Link = {
@@ -14,11 +16,21 @@ const Navbar = () => {
     { title: "Speakers", href: "#" },
     { title: "Earphones", href: "#" },
   ];
+
+  const [isOpen, setOpen] = useState(false);
+
   return (
     <div className="w-full h-auto sm:px-10 fixed z-50">
-      <nav className="w-full sm:px-0 px-6 py-8 max-w-[1110px] mx-auto bg-transparent border-b-[1px] border-zinc-700 flex items-center justify-between relative">
-        <Logo />
-
+      <nav className="w-full sm:px-0 pl-2 pr-6 sm:py-8 py-6 max-w-[1110px] mx-auto bg-transparent border-b-[1px] border-zinc-700 flex items-center justify-between relative">
+        <div className="block lg:hidden">
+          <Hamburger
+            toggled={isOpen}
+            toggle={setOpen}
+            color="#FFFFFF"
+            size={20}
+          />
+        </div>
+        <Logo className="lg:static absolute sm:left-20 sm:right-auto sm:mx-0 left-0 right-0 mx-auto" />
         <div className="w-full max-w-[429px] absolute left-0 right-0 mx-auto lg:flex hidden items-center gap-x-[34px]  ">
           {links.map((link, index) => (
             <Link
