6547038b3a1ea1f09b1d6279663c2ae438191c4e
Kaiser <kaiserzulkarnain@gmail.com>
2024-04-25 19:10:21 +0700

    feat: xx99-markII-headphones page update

--- src/components/layout/product-detail.tsx
+++ src/components/layout/product-detail.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Image from "next/image";
 import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
 
-interface ProductDescription {
+export interface ProductDescription {
   name: string;
   desc: string;
   image: string;
