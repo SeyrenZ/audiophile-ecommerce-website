4cbd3c749ed9da5c8fd4b177bfa26632e556190f
Kaiser <kaiserzulkarnain@gmail.com>
2024-04-26 18:38:07 +0700

    refactor: change file name in public folder

--- src/app/headphones/xx99-markII-headphones/page.tsx
+++ src/app/headphones/xx99-markII-headphones/page.tsx
@@ -9,9 +9,9 @@ const Home = () => {
     name: "XX99 MARK II HEADPHONES",
     desc: "The new XX99 Mark II headphones is the pinnacle of pristine audio. It redefines your premium headphone experience by reproducing the balanced depth and precision of studio-quality sound.",
     image: "/xx99-markII-product-photo.svg",
-    subImage1: "/xx99-markII-product-detail-image1.svg",
-    subImage2: "/xx99-markII-product-detail-image2.svg",
-    subImage3: "/xx99-markII-product-detail-image3.svg",
+    subImage1: "/xx99-markII-sub-image-1.svg",
+    subImage2: "/xx99-markII-sub-image-2.svg",
+    subImage3: "/xx99-markII-sub-image-3.svg",
     features:
       "Featuring a genuine leather head strap and premium earcups, these headphones deliver superior comfort for those who like to enjoy endless listening. It includes intuitive controls designed for any situation. Whether you’re taking a business call or just in your own personal space, the auto on/off and pause features ensure that you’ll never miss a beat  The advanced Active Noise Cancellation with built-in equalizer allow you to experience your audio world on your terms. It lets you enjoy your audio in peace, but quickly interact with your surroundings when you need to. Combined with Bluetooth 5. 0 compliant connectivity and 17 hour battery life, the XX99 Mark II headphones gives you superior sound, cutting-edge technology, and a modern design aesthetic.",
     newProduct: "visible",
