0f1890becf8fc3eafc856b559c92655632e0996b
Kaiser <kaiserzulkarnain@gmail.com>
2024-04-26 20:12:05 +0700

    refactor: fix typo for image alt for zx9 and zx7 speakers

--- src/app/speakers/zx7-speaker/page.tsx
+++ src/app/speakers/zx7-speaker/page.tsx
@@ -17,7 +17,7 @@ const Home = () => {
     features:
       "Reap the advantages of a flat diaphragm tweeter cone. This provides a fast response rate and excellent high frequencies that lower tiered bookshelf speakers cannot provide. The woofers are made from aluminum that produces a unique and clear sound. XLR inputs allow you to connect to a mixer for more advanced usage.The ZX7 speaker is the perfect blend of stylish design and high performance. It houses an encased MDF wooden enclosure which minimises acoustic resonance. Dual connectivity allows pairing through bluetooth or traditional optical and RCA input. Switch input sources and control volume at your finger tips with the included wireless remote. This versatile speaker is equipped to deliver an authentic listening experience.",
     newProduct: "invisible",
-    alt: "XX59-HEADPHONES",
+    alt: "ZX7-SPEAKER",
     price: "4,500", // put as string to avoid comma for now
     itemsInBox: [
       { quantity: 2, unit: "Speaker Unit" },
