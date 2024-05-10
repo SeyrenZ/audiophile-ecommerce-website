6b17d0bee426e1d4b12e58c4a12a8a227c2f4954
Kaiser <kaiserzulkarnain@gmail.com>
2024-05-11 01:49:39 +0700

    refactor: change headphones product id name

--- src/app/headphones/[productId]/page.tsx
+++ src/app/headphones/[productId]/page.tsx
@@ -1,10 +1,10 @@
-'use client'
-import { useState, useEffect } from "react"
-import { useParams } from "next/navigation"
-import ProductDetail from "@app/components/layout/product-detail"
+"use client";
+import { useState, useEffect } from "react";
+import { useParams } from "next/navigation";
+import ProductDetail from "@app/components/layout/product-detail";
 
 type ProductDescription = {
-  id: number;
+  id: string;
   name: string;
   desc: string;
   image: string;
@@ -16,14 +16,12 @@ type ProductDescription = {
   alt: string;
   price: string;
   itemsInBox: { quantity: number; unit: string }[];
-}
-
-
+};
 
 const Page = () => {
   const [productData, setProductData] = useState<ProductDescription[]>([]);
   const params = useParams();
-  console.log(params)
+  console.log(params);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -40,12 +38,16 @@ const Page = () => {
   }, []);
   return (
     <div>
-    <div className="w-full lg:h-[90px] sm:h-[113px] h-[97px] bg-black" />
-    <div className="lg:py-[160px] py-[65px] space-y-[160px]">
-    {productData.filter(product => product.id === Number(params.productId)).map((product, index) => <ProductDetail key={index} {...product} />)}
+      <div className="w-full lg:h-[90px] sm:h-[113px] h-[97px] bg-black" />
+      <div className="lg:py-[160px] py-[65px] space-y-[160px]">
+        {productData
+          .filter((product) => product.id === String(params.productId))
+          .map((product, index) => (
+            <ProductDetail key={index} {...product} />
+          ))}
+      </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default Page
+export default Page;
