ce779ea0a55831dc8cc38651bb56a16014416088
Kaiser <kaiserzulkarnain@gmail.com>
2024-05-13 18:45:03 +0700

    feat: add some input field in the form of checkout page

--- src/components/checkout/checkout-form.tsx
+++ src/components/checkout/checkout-form.tsx
@@ -39,29 +39,239 @@ const CheckoutForm = () => {
   return (
     <div className="w-full h-full container">
       <div className="w-full h-full max-w-[1110px] mx-auto flex justify-between">
-        <div className="w-full max-w-[730px] h-[1126px] px-12 py-14 bg-white rounded-lg">
+        <div className="w-full max-w-[730px] h-auto min-h-[1126px] px-12 py-14 bg-white rounded-lg flex flex-col gap-y-[41px]">
           <div className="text-[32px] leading-[38px] font-bold tracking-[1.14px]">
             CHECKOUT
           </div>
           <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-              <FormField
-                control={form.control}
-                name="username"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Username</FormLabel>
-                    <FormControl>
-                      <Input placeholder="shadcn" {...field} />
-                    </FormControl>
-                    <FormDescription>
-                      This is your public display name.
-                    </FormDescription>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <Button type="submit">Submit</Button>
+            <form
+              onSubmit={form.handleSubmit(onSubmit)}
+              className="flex flex-col gap-y-[41px]"
+            >
+              <div className="flex flex-col gap-y-4">
+                <div className="text-[13px] leading-[25px] tracking-[0.93px] font-bold text-primary-copper">
+                  BILLING DETAILS
+                </div>
+                <div className="w-full grid grid-cols-2 gap-x-4 gap-y-6">
+                  <FormField
+                    control={form.control}
+                    name="username"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Name</FormLabel>
+                        <FormControl>
+                          <Input
+                            placeholder="Alexai Ward"
+                            {...field}
+                            className="px-6 py-[28px] border-[1px] border-primary-whiteSmoke rounded-lg"
+                          />
+                        </FormControl>
+                        <FormDescription></FormDescription>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={form.control}
+                    name="username"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Email Address</FormLabel>
+                        <FormControl>
+                          <Input
+                            placeholder="alexai@gmail.com"
+                            {...field}
+                            className="px-6 py-[28px] border-[1px] border-primary-whiteSmoke rounded-lg"
+                          />
+                        </FormControl>
+                        <FormDescription></FormDescription>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={form.control}
+                    name="username"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Phone Number</FormLabel>
+                        <FormControl>
+                          <Input
+                            placeholder="+1 202-555-0136"
+                            {...field}
+                            className="px-6 py-[28px] border-[1px] border-primary-whiteSmoke rounded-lg"
+                          />
+                        </FormControl>
+                        <FormDescription></FormDescription>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                </div>
+              </div>
+
+              <div className="flex flex-col gap-y-4">
+                <div className="text-[13px] leading-[25px] tracking-[0.93px] font-bold text-primary-copper">
+                  SHIPPING DETAILS
+                </div>
+                <div className="w-full grid grid-cols-2 gap-x-4 gap-y-6">
+                  <div className="col-span-2">
+                    <FormField
+                      control={form.control}
+                      name="username"
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel>Address</FormLabel>
+                          <FormControl>
+                            <Input
+                              placeholder="1137 Williams Avenue"
+                              {...field}
+                              className="px-6 py-[28px] border-[1px] border-primary-whiteSmoke rounded-lg"
+                            />
+                          </FormControl>
+                          <FormDescription></FormDescription>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+                  </div>
+                  <FormField
+                    control={form.control}
+                    name="username"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>ZIP Code</FormLabel>
+                        <FormControl>
+                          <Input
+                            placeholder="10001"
+                            {...field}
+                            className="px-6 py-[28px] border-[1px] border-primary-whiteSmoke rounded-lg"
+                          />
+                        </FormControl>
+                        <FormDescription></FormDescription>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={form.control}
+                    name="username"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>City</FormLabel>
+                        <FormControl>
+                          <Input
+                            placeholder="New York"
+                            {...field}
+                            className="px-6 py-[28px] border-[1px] border-primary-whiteSmoke rounded-lg"
+                          />
+                        </FormControl>
+                        <FormDescription></FormDescription>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={form.control}
+                    name="username"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Country</FormLabel>
+                        <FormControl>
+                          <Input
+                            placeholder="United States"
+                            {...field}
+                            className="px-6 py-[28px] border-[1px] border-primary-whiteSmoke rounded-lg"
+                          />
+                        </FormControl>
+                        <FormDescription></FormDescription>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                </div>
+              </div>
+
+              <div className="flex flex-col gap-y-4">
+                <div className="text-[13px] leading-[25px] tracking-[0.93px] font-bold text-primary-copper">
+                  SHIPPING DETAILS
+                </div>
+                <div className="w-full grid grid-cols-2 gap-x-4 gap-y-6">
+                  <div className="row-span-2">Payment method</div>
+                  <FormField
+                    control={form.control}
+                    name="username"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Address</FormLabel>
+                        <FormControl>
+                          <Input
+                            placeholder="1137 Williams Avenue"
+                            {...field}
+                            className="px-6 py-[28px] border-[1px] border-primary-whiteSmoke rounded-lg"
+                          />
+                        </FormControl>
+                        <FormDescription></FormDescription>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={form.control}
+                    name="username"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>ZIP Code</FormLabel>
+                        <FormControl>
+                          <Input
+                            placeholder="10001"
+                            {...field}
+                            className="px-6 py-[28px] border-[1px] border-primary-whiteSmoke rounded-lg"
+                          />
+                        </FormControl>
+                        <FormDescription></FormDescription>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={form.control}
+                    name="username"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>City</FormLabel>
+                        <FormControl>
+                          <Input
+                            placeholder="New York"
+                            {...field}
+                            className="px-6 py-[28px] border-[1px] border-primary-whiteSmoke rounded-lg"
+                          />
+                        </FormControl>
+                        <FormDescription></FormDescription>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={form.control}
+                    name="username"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Country</FormLabel>
+                        <FormControl>
+                          <Input
+                            placeholder="United States"
+                            {...field}
+                            className="px-6 py-[28px] border-[1px] border-primary-whiteSmoke rounded-lg"
+                          />
+                        </FormControl>
+                        <FormDescription></FormDescription>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                </div>
+              </div>
             </form>
           </Form>
         </div>
